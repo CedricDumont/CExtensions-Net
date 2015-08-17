7c93366701ba8f3393e6a3ac25fdd86ea7b96798
Cedric Dumont <cedric.dumont.ext@atos.net>
2015-08-17 12:28:09 +0200

    added test project for EntityFramework

---
+++ src/CExtensions.EntityFramework.Test/DBContextExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Shouldly;
+using CExtensions.Effort.SampleApp;
+using CExtensions.EF;
+using CExtensions.Effort;
+
+namespace CExtensions.EntityFramework.Test
+{
+    public class DBContextExtensionsTest
+    {
+        private string _modelNamespace = "CExtensions.Effort.SampleApp";
+
+        [Fact]
+        public void TestMethod1()
+        {
+            using (SampleContext context = new XmlFileContext<SampleContext>(this.GetType()).Empty())
+            {
+                var result = context.DbSets(_modelNamespace);
+
+                result.Count.ShouldBe(2);
+            }
+        }
+    }
+}
